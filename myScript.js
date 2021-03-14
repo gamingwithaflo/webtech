41fd13cd72fe47119b2549b14838debe1a47d7b3
Jaylan Lee <j.s.c.lee@students.uu.nl>
2021-03-14 20:23:13 +0100

    Update myScript.js
    changed name and formatting

--- myScript.js
+++ myScript.js
@@ -1,11 +1,10 @@
-   window.addEventListener("load", registerEvents, false);
-   
-   function toggleDisplay () {
-        let content = document.getElementById("menu-conetent-option-notable-features");
-        content.classList.toggle("menu-content-option__show");
-   }
+window.addEventListener("load", registerEvents, false);
 
-   function registerEvents(){
- document.getElementById("virtual-dom").addEventListener("click", toggleDisplay, false);
-   }
- 
+function toggleDisplay() {
+ let content = document.getElementById("menu-conetent-option-notable-features");
+ content.classList.toggle("menu-content-option__show");
+}
+
+function registerEvents() {
+ document.getElementById("menu-notable-features").addEventListener("click", toggleDisplay, false);
+}
