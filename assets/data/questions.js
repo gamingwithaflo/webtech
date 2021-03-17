b7ea287dd22068b6c8a96d3af72a2c29aee72370
Jaylan Lee <j.s.c.lee@students.uu.nl>
2021-03-17 22:52:28 +0100

    Update questions.js
    new questions

--- assets/data/questions.js
+++ assets/data/questions.js
@@ -3,45 +3,50 @@ const questionsObject = {
     {
       "type": "multiplechoice",
       "title": "Question 1",
-      "problemStatement": "Is this a boolean question?",
-      "correctAnswer": "true",
+      "problemStatement": "The lifecycle methods are mainly used for ___.",
+      "correctAnswer": "keeping track of event history",
       "answers": [
-        "true",
-        "false"
+        "keeping track of event history",
+        "enhancing components",
+        "freeing up resources",
+        "none of the above"
       ]
     },
     {
       "type": "multiplechoice",
       "title": "Question 2",
-      "problemStatement": "What is the subject of this website?",
-      "correctAnswer": "react",
+      "problemStatement": "React is mainly used for building ___.",
+      "correctAnswer": "User Interface",
       "answers": [
-        "angular",
-        "vue",
-        "react"
+        "Database",
+        "Connectivity",
+        "User Interface",
+        "Design Platform"
       ]
     },
     {
       "type": "open",
       "title": "Question 3",
-      "problemStatement": "What is the name of React?",
-      "correctAnswer": "react"
+      "problemStatement": "What is used to pass data to a component from outside?",
+      "correctAnswer": "setState"
     },
     {
       "type": "multiplechoice",
       "title": "Question 4",
-      "problemStatement": "Is React used for building interfaces?",
-      "correctAnswer": "yes",
+      "problemStatement": "Which is the newest version of React.js?",
+      "correctAnswer": "17.0.1",
       "answers": [
-        "yes",
-        "no"
+        "17.0.1",
+        "17.0.0",
+        "16.0.1",
+        "17.0.4"
       ]
     },
     {
       "type": "open",
       "title": "Question 5",
-      "problemStatement": "Type something",
-      "correctAnswer": "something"
+      "problemStatement": "___ can be done while multiple elements need to be returned from a component.",
+      "correctAnswer": "Packing"
     }
   ]
 }
