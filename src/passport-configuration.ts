4f966d8d9ae64c9dab769568f09b94df400329fc
Jaylan Lee <j.s.c.lee@students.uu.nl>
2021-03-31 11:44:58 +0200

    login serialize fixed
    vrij convoluted gedaan maar het werkt. ik ga kijken of het slanker kan

--- src/passport-configuration.ts
+++ src/passport-configuration.ts
@@ -1,5 +1,5 @@
 /* eslint @typescript-eslint/no-var-requires: "off" */
-const LocalStratagy = require("passport-local").Strategy;
+const LocalStrategy = require("passport-local").Strategy;
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("./db/webtech.db");
 
@@ -8,19 +8,38 @@ function StartupPassport(passport: any, getUserById: any) {
     db.get("select * from user where mail_address = ? and password = ?", [email, password], (err: any, result: any) => {
       if(err) {
         console.log(err);
+        return done(err);
       } else if (typeof result == 'undefined'){    // no user found (inside database);
-        done(null, false, { message: "No user with that email or incorrect password" });
+        console.log("no find");
+        return done(null, false, { message: "No user with that email or incorrect password" });
       } else {    // user found (inside database);
-        done(null, result);
+        console.log("yes find");
+        console.info(result);
+        return done(null, result);
       }
     });
   };
+
+  const use = (serializeAfter: any) => {
+    passport.use(new LocalStrategy({ usernameField: "email" }, identifyUser));
+    serializeAfter(deserialize);
+  }
+
+  const serialize = (deserializeAfter: any) => {
+    passport.serializeUser((user: any, done: any) => done(null, user.user_id));
+    deserializeAfter();
+  }
+
+  const deserialize = () => {
+    passport.deserializeUser((id: any, done: any) => {
+      console.log(id);
+      return done(null, getUserById(id));
+    });
+  }
+
+  use(serialize);
   // because our default username is not equal to user we need to change that (but password default is allready good)
-  passport.use(new LocalStratagy({ usernameField: "email" }, identifyUser));
-  passport.serializeUser((user: any, done: any) => done(null, user.id));
-  passport.deserializeUser((id: any, done: any) => {
-    return done(null, getUserById(id));
-  });
+
 }
 
 export default StartupPassport;
